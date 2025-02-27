dbeb68d7e74e5f018d3223ed478faa9618b7c2e6
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-02-27 14:46:56 -0500

    comment changes

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.method === "getText") {
-        // Send a message to the active tab
+        // send a message to the active tab
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, { method: "getText" }, sendResponse);
         });
-        return true; // Keep the message channel open for async response
+        return true; // keep the message channel open for async response
     }
 });
 chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch((error) => console.error(error));
--- popup/popup.js
+++ popup/popup.js
@@ -4,11 +4,13 @@ function summarizeContent(articleText) {
     document.getElementById("outputArea").value = response.content;
 }
 
+// I actually think the above code isn't needed, use backend for summarization API
+
 document.querySelector('.button.is-success').addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { action: "getArticleContent" }, (response) => {
             if (response && response.content) {
-                // response.content <-- content
+                // send the content to the backend for summarization
                 document.getElementById("outputArea").value = response.content;
             } else {
                 document.getElementById("outputArea").value = "No content found. Please use a valid website or refresh the page and try again.";
