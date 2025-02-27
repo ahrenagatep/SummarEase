234a5c5bce04b9bc7c95ce252223eb6d971f8ecd
Ahren Agatep <36459220+ahrenagatep@users.noreply.github.com>
2025-02-27 14:37:56 -0500

    changed comments

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 console.log("Content script loaded");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getArticleContent") {
-        // grabs all paragraph tags on the page
-        const paragraphs = document.querySelectorAll('p'); // Adjust selector as needed
+    if (request.action === "getArticleContent") {           // grabs all paragraph tags <p> on the page
+        const paragraphs = document.querySelectorAll('p');  // adjust selector as needed
         let articleText = '';
 
         paragraphs.forEach(paragraph => {
